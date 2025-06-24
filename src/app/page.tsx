31ac4227e264ae5c20ed37f2d4893d6610a9dcb6
Thomas Kone <95449179+kone24@users.noreply.github.com>
2025-06-24 12:59:40 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <h1 className="max-w-2/3 text-4xl font-bold leading-relaxed">
+      <h1 className="max-w-2/3 text-4xl font-bold">
       Welcome to KLM Labs, my personal playground.
-      <span className="block text-xl font-normal mt-2">
+      <span className="block text-3xl font-normal mt-2 leading-relaxed">
         Here you’ll find an evolving collection of my work — spanning web and app development, health tech experiments, photography, music, and more. I’m constantly tinkering with new ideas, from turning personal health data into actionable insights to capturing life’s moments through sound and image. My goal? To be good at everything, or at least try to be. 
       </span>
     </h1>
